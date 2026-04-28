721ee7cc474ef8a86e47ba4128a6be569bb5322a
AnthonyRova <anthony@getrova.com>
2026-04-28 15:59:06 +0100

    Added new categories and reduced maximum amount of questions per request

--- js/app.js
+++ js/app.js
@@ -5,11 +5,28 @@ const categories = [
   { id: 10, name: "Books" },
   { id: 11, name: "Film" },
   { id: 12, name: "Music" },
+  { id: 13, name: "Musicals & Theatres"},
+  { id: 14, name: "Television"},
+  { id: 15, name: "Video Games"},
+  { id: 16, name: "Board Games"},
   { id: 17, name: "Science & Nature" },
   { id: 18, name: "Computers" },
+  { id: 19, name: "Mathematics" },
+  { id: 20, name: "Mythology" },
   { id: 21, name: "Sports" },
+  { id: 22, name: "Geography" },
+  { id: 24, name: "Politics" },
+  { id: 25, name: "Art" },
+  { id: 26, name: "Celebrities" },
+  { id: 27, name: "Animals" },
+  { id: 28, name: "Vehicles" },
+  { id: 29, name: "Comics" },
+  { id: 30, name: "Gadgets" },
+  { id: 31, name: "Japanese Anime & Manga" },
+  { id: 32, name: "Cartoon & Animations" },
   { id: 23, name: "History" }
 ];
+
 
 // DOM
 const quizScreen = document.getElementById("quiz-screen");
